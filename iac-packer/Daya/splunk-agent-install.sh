1d0b1a24e2686b6023de740768992a7f14f19f9c
dayanidhi417 <b.dayanidhi@gmail.com>
2020-07-20 05:38:37 -0700

    latest update

--- iac-packer/Daya/splunk-agent-install.sh
+++ iac-packer/Daya/splunk-agent-install.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -e
+#getting splunkforwarder package
+wget -O splunkforwarder-8.0.5-a1a6394cc5ae-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.5&product=universalforwarder&filename=splunkforwarder-8.0.5-a1a6394cc5ae-linux-2.6-amd64.deb&wget=true'
+sudo apt install ./splunkforwarder-8.0.5-a1a6394cc5ae-linux-2.6-amd64.deb
+#sudo chmod o+rwx /opt/splunkforwarder
+#cd /opt/splunkforwarder/bin/
+#sudo ./splunk start
+#sudo ./splunk status
 
-echo "------------ Install NewRelic --------------"
-
-#Create a configuration file and add your license key
-echo "license_key: cb60ff5db7be50e6f092570341744c973f0dNRAL" | sudo tee -a /etc/newrelic-infra.yml
-
-#Enable New Relic’s GPG key
-curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
-
-#Create the agent’s apt repo using the command for your distribution version
-printf "deb [arch=amd64] http://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
-
-#Update your apt cache
-sudo apt-get update
-
-#Run the install script
-sudo apt-get install newrelic-infra -y
