3820e4aa28c057f682ac33c7c189bfbf4718f9e5
sivashankarbehera <sivashankarbehera1@gmail.com>
2020-07-21 11:05:34 +0530

    Jenkins

--- iac-packer/siva/jenkins.sh
+++ iac-packer/siva/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt install default-jre
+sudo apt install default-jre -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
