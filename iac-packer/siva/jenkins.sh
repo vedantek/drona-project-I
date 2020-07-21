dbfb9ea7e9df0bc819547be8f20fc1141f3e6fb6
sivashankarbehera <sivashankarbehera1@gmail.com>
2020-07-21 13:16:48 +0530

    jenkins

--- iac-packer/siva/jenkins.sh
+++ iac-packer/siva/jenkins.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
 sudo apt-get install jenkins -y
-systemctl enable jenkins service
-systemctl start jenkins service
+sudo apt-get update
+service jenkins enable
+service jenkins start
