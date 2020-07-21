7842684e8df4ef26aa8be341a2f2fcfcf7694213
Sreenath <sreenu.vrinda@gmail.com>
2020-07-21 18:49:58 +0530

    sreenath

--- iac-packer/sreenath/sreenath_nodejs.sh
+++ iac-packer/sreenath/sreenath_nodejs.sh
@@ -6,13 +6,10 @@ echo "------------ Install NodeJS--------------"
 #Insall apt update
 sudo apt-get update
 
-#Install NodeJS
-sudo apt-get install nodejs
-
 #Fix broken dependencies
 sudo apt-get install -f
 
-#Try Installing NodeJS again
+#Install NodeJS
 sudo apt-get install nodejs
 
 #Check the NodeJS Version
