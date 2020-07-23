e516fa0398f094a89661d15c211f5ef83fa155b8
Sreenath <sreenu.vrinda@gmail.com>
2020-07-23 12:51:57 +0530

    sreenath

--- iac-packer/sreenath/sreenath_nodejs.sh
+++ iac-packer/sreenath/sreenath_nodejs.sh
@@ -6,15 +6,15 @@ echo "------------ Install NodeJS--------------"
 #Insall apt update
 sudo apt-get update
 
-sudo apt-get install aptitude -y
-sudo aptitude install libc-ares2 -y
-sudo aptitude install libhttp-parser2.7.1 -y
+#sudo apt-get install aptitude -y
+#sudo aptitude install libc-ares2 -y
+#sudo aptitude install libhttp-parser2.7.1 -y
 
 #Fix broken dependencies
 #sudo apt-get install -f
 
-#Install NodeJS
-sudo apt-get install nodejs -y
+#Install NodeJS and npm
+sudo apt-get install nodejs -y && sudo apt-get install npm -y
 
 #Check the NodeJS Version
 node –v
