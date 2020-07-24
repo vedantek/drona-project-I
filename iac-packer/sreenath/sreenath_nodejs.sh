5ff12a7940f2a5a3950141a30768ebff00360517
Sreenath <sreenu.vrinda@gmail.com>
2020-07-24 13:36:15 +0530

    sreenath

--- iac-packer/sreenath/sreenath_nodejs.sh
+++ iac-packer/sreenath/sreenath_nodejs.sh
@@ -14,12 +14,13 @@ sudo apt-get update
 #sudo apt-get install -f
 
 #Install NodeJS and npm
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #&& sudo apt-get install -y npm
 
 #Check the NodeJS Version
 node –v
-
 #npm version
-#npm –version
+npm –v
