09926af71b2ad6ecb50e9c5fec9272d73679aa6b
Sreenath <sreenu.vrinda@gmail.com>
2020-07-24 23:37:07 +0530

    sree

--- iac-packer/sreenath/sreenath_nodejs.sh
+++ iac-packer/sreenath/sreenath_nodejs.sh
@@ -4,7 +4,7 @@ set -e
 echo "------------ Install NodeJS--------------"
 
 #Insall apt update
-sudo apt-get update
+sudo apt-get update -y
 
 #sudo apt-get install aptitude -y
 #sudo aptitude install libc-ares2 -y
@@ -14,7 +14,7 @@ sudo apt-get update
 #sudo apt-get install -f
 
 #Install NodeJS and npm
-sudo apt-get install curl
+sudo apt-get install -y curl
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
