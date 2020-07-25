84e1e713eb7768a7ec000a37b5b695f101e029f9
asutoshp1 <asutoshp1@gmail.com>
2020-07-25 12:29:44 +0530

    new relic and jenkins

--- iac-packer/asutosh/newrelic-agent.sh
+++ iac-packer/asutosh/newrelic-agent.sh
@@ -3,19 +3,19 @@ set -e
 
 echo "------------ Install NewRelic --------------"
 
-Create a configuration file and add your license key
+#Create a configuration file and add your license key
 echo "license_key: cb60ff5db7be50e6f092570341744c973f0dNRAL" | sudo tee -a /etc/newrelic-infra.yml
 
-Enable New Relic’s GPG key
+#Enable New Relic’s GPG key
 curl https://download.newrelic.com/infrastructure_agent/gpg/newrelic-infra.gpg | sudo apt-key add -
 
-Create the agent’s apt repo using the command for your distribution version
+#Create the agent’s apt repo using the command for your distribution version
 printf "deb [arch=amd64] http://download.newrelic.com/infrastructure_agent/linux/apt bionic main" | sudo tee -a /etc/apt/sources.list.d/newrelic-infra.list
 
-Update your apt cache
+#Update your apt cache
 sudo apt-get update
 
-Run the install script
+#Run the install script
 sudo apt-get install newrelic-infra -y
 
 sudo apt-get update
